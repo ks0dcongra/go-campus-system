ad3a9bc41c8204e63c5d604843ff1caebd03d412
ks0dcongra <ks0dcongra@gmail.com>
2023-03-17 12:21:58 +0800

    feat: delete redundant struct in student model

--- app/model/student_model.go
+++ app/model/student_model.go
@@ -9,8 +9,6 @@ type Student struct {
 	Name string `binding:"required,gte=2"`
 	Password string `binding:"required"`
 	Student_number string `binding:"required"`
-	// Course []Course `gorm:"many2many:scores;References:Id;joinReferences:Id"`
-	// Score []Score `gorm:"foreignKey:Id"`
 	// Course []Course `gorm:"many2many:score;ForeignKey:Id;joinForeignKey:Student_id;References:Id;joinReferences:Course_id"`
 	CreatedTime time.Time
 	UpdatedTime time.Time
@@ -33,40 +31,4 @@ type ReturnStudent struct {
 		Name string  `binding:"required"`
 		Subject string `binding:"required"`
 		Score int `binding:"required"`
-}
-
-
-// type Happy struct {
-// 	Name string  `binding:"required"`
-// 	Subject string `binding:"required"`
-// 	Score int `binding:"required"`
-// }
-
-// type ReturnStudent struct {
-// 	Name Array  `binding:"required"`
-// 	Subject string `binding:"required"`
-// 	Score int `binding:"required"`
-// }
-// type Name struct {
-// 	Name string
-// }
-
-// type Score2 struct {
-// 	Score int
-// }
-
-// type Subject struct {
-// 	Subject string
-// }
-
-type ReturnStudent2 struct {
-	Name string  `binding:"required"`
-	Subject string `binding:"required"`
-	Score int `binding:"required"`
-}
-
-type User struct {
-	Name string  `binding:"required"`
-	Subject string `binding:"required"`
-	Score int `binding:"required"`
 }
