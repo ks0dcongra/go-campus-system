85845532d66f7deaf8b1e6655816bab5fc88b815
ks0dcongra <ks0dcongra@gmail.com>
2023-04-14 18:16:25 +0800

    docs: add annotation for session function in middleware

--- app/http/middleware/session.go
+++ app/http/middleware/session.go
@@ -1,10 +1,11 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 const userkey = "session_id"
@@ -17,9 +18,11 @@ func SetSession() gin.HandlerFunc {
 // User Auth Session Middle
 func AuthSession() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 拿到我在 userApi := router.Group("user/api", session.SetSession())所暫存在Default變數的session
 		session := sessions.Default(c)
-
+		// 拿到我在session所暫存的ID
 		sessionID := session.Get(userkey)
+		// 如果我所暫存的變數不存在代表該人沒有登入過
 		if sessionID == nil {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 				"message": "此頁面需要登入",
@@ -31,15 +34,21 @@ func AuthSession() gin.HandlerFunc {
 
 // Save Session for User
 func SaveSession(c *gin.Context, userID int) {
+	// 拿到我在 userApi := router.Group("user/api", session.SetSession())所暫存在Default變數的session
 	session := sessions.Default(c)
+	// 用session來暫存userID
 	session.Set(userkey, userID)
+	// 將當下登入的sesison存入伺服器端
 	session.Save()
 }
 
 // Clear Session for User
 func ClearSession(c *gin.Context) {
+	// 拿到我在 userApi := router.Group("user/api", session.SetSession())所暫存在Default變數的session
 	session := sessions.Default(c)
+	// 將目前session清除
 	session.Clear()
+	// 將當下登出的sesison存入伺服器端
 	session.Save()
 }
 
