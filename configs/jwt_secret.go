cd62ad6775055b3fd18dbb3f2c3da6a22bf80418
andy_101398 <andy_101398@castech.com.tw>
2023-05-17 10:30:32 +0800

    test: test donre trigger from config/jwt_secret

--- configs/jwt_secret.go
+++ configs/jwt_secret.go
@@ -1,5 +1,5 @@
 package configs
 
 const (
-	JWTSecret string = "b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn"
+	JWTSecret string = "b4BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn"
 )
