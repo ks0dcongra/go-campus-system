37f7f12a5916dff1c7f4733cafa9007c61fba74a
andy_101398 <andy_101398@castech.com.tw>
2023-06-26 17:11:04 +0800

    feat:add AM server regex with positive lookahead

--- app/http/middleware/validator.go
+++ app/http/middleware/validator.go
@@ -6,10 +6,9 @@ import (
 )
 
 func UserPasd(field validator.FieldLevel) bool {
-	// 右邊表達式為開頭必為大寫且長度最小為五最大為十：^[A-Z]\w{4,10}$
-	// 下面表達式為
-	if match, _ := regexp.MatchString(`^[a-zA-Z0-9_!@]+$`, field.Field().String()); match {
-		return true
+	// 右邊表達式為開頭必為大寫且長度最小為五最大為十：^[A-Z]\w{4,10}$，右邊表達式為^[a-zA-Z0-9_!@]+$
+	if match, _ := regexp.MatchString(`^(.*(?:\b[Aa][Nn][Dd]\b|\b[Oo][Rr]\b|\b[Ss][Ee][Ll][Ee][Cc][Tt]\b|\b[Ww][Hh][Ee][Rr][Ee]\b|\b[Hh][Aa][Vv][Ii][Nn][Gg]\b|\b[Uu][Nn][Ii][Oo][Nn]\b)|.*\d+=\d+|.*[a-zA-Z]+=[a-zA-Z]+|.*--|\s)+.*$`, field.Field().String()); match {
+		return false
 	}
-	return false
+	return true
 }
